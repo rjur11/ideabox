f68ce3ea52a40807b855a2ec089669e6dddee831
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-02-01 13:27:39 -0500

    Create ideabox

---
+++ src/Ideas.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import "./Ideas.css";
+import Card from "./Card";
+
+const Ideas = ({ ideas, deleteIdea }) => {
+  const ideaCards = ideas.map((idea) => {
+    return (
+      <Card
+        title={idea.title}
+        description={idea.description}
+        id={idea.id}
+        key={idea.id}
+        deleteIdea={deleteIdea}
+      />
+    );
+  });
+
+  return <div className="ideas-container">{ideaCards}</div>;
+};
+
+export default Ideas;
